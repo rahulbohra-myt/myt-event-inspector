05210346ceeb07f38d566fd12888e18d0570de85
Rahul Bohra <rahul.bohra@myyogateacher>
2026-05-28 22:24:08 +0530

    Remove dist and node_modules from tracking

--- dist/assets/index.js-BQjvCs1K.js
+++
@@ -1,2 +0,0 @@
-(function(){window.addEventListener("message",a=>{a.source===window&&(!a.data||a.data.type!=="MYT_TIMELINE_EVENT"||chrome.runtime.sendMessage({type:"MYT_TIMELINE_EVENT",payload:a.data.payload}))});
-})()
